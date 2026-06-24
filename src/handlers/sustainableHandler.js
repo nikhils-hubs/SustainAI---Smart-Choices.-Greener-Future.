8946ea0dc6bb58c7fd9130def8e84252db5e7066
Nikhil singh <nikhilsingh1696@gmail.com>
2026-06-24 22:27:11 +0530

    completed whole projects and fixed some bugs

--- src/handlers/sustainableHandler.js
+++ src/handlers/sustainableHandler.js
@@ -2,7 +2,7 @@ const generateResponse = require('../services/aiServices')
 const sustainablePrompt = require('../prompts/sustainablePrompt')
 
 async function sustainbaleHandler(histroy) {
-    const reply = await generateResponse(sustainablePrompt, history)
+    const reply = await generateResponse(sustainablePrompt, histroy)
     return reply;
 }
 module.exports = sustainbaleHandler;
